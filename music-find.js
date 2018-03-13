c0f0d561b591e003803dbfcf26d8a8d47462335b
dschurman <dschurman1@gmail.com>
2018-03-13 15:52:27 -0400

    nvm just added an alert...

--- music-find.js
+++ music-find.js
@@ -312,7 +312,7 @@ function get_artist_id(artist_name, callback, args) {
         if (err) { console.error(err); }
         else { 
           console.log(data)
-          if(data == null) {
+          if(data.artists.items == []) {
             alert("Artist not found");
           } else {
             callback(data.artists.items[0].id, args); 
